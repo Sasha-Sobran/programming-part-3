e2060671ccfa5cd2551239d93f7cf47f3517b96b
Sasha <113774476+Sasha-Sobran@users.noreply.github.com>
2023-10-11 21:35:56 +0300

    added test

--- main.py
+++ main.py
@@ -10,16 +10,14 @@ def can_place_cows(free_sections, C, min_distance):
     return cows_placed >= C
 
 
-def get_max_width(N=9975, C=25, free_sections=[i for i in range(100000)]):
+def get_max_width(N=5, C=3, free_sections=[1, 2, 8, 4, 9]):
     """
     :param N: count of cows
     :param C: count of aggressive cows
     :param free_sections: free sections
     :return: maximal minimal width
     """
-    if N+C > len(free_sections):
-        return 0
-    elif C == 2:
+    if C == 2:
         return max(free_sections) - min(free_sections)
 
     free_sections.sort()
--- test.py
+++ test.py
@@ -1,25 +1,12 @@
 import unittest
 
-from main import get_longest_peak_subsequence_length
+from main import get_max_width
 
 
-class TestGetLongestSubsequenceLen(unittest.TestCase):
+class GetMaxWidthTest(unittest.TestCase):
 
-    def test_sorted_array(self):
-        self.assertEqual(get_longest_peak_subsequence_length([1, 2, 3, 4, 5]), 0)
-
-    def test_reverse_sorted_array(self):
-        self.assertEqual(get_longest_peak_subsequence_length([5, 4, 3, 2, 1]), 0)
-
-    def test_two_element_array(self):
-        self.assertEqual(get_longest_peak_subsequence_length([1, 2]), 0)
-
-    def test_no_peak_subsequence(self):
-        self.assertEqual(get_longest_peak_subsequence_length([-1, - 5, - 1]), 0)
-
-    def test_three_peak_subsequence(self):
-        self.assertEqual(
-            get_longest_peak_subsequence_length([1, 3, 5, 4, 8, 3, 7, 1, 3, 4, 2, 8, 3, 7, 1, 5, 4, 8, 3, 7]), 4)
+    def test(self):
+        self.assertEqual(get_max_width(5, 3, [1, 2, 8, 4, 9]), 3)
 
 
 if __name__ == "__main__":
