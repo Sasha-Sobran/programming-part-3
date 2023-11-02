c8c28b421c72c80058a66be5078a1438a4fe981a
Sasha <113774476+Sasha-Sobran@users.noreply.github.com>
2023-11-02 11:40:10 +0200

    add fourth lab and work in laboratory

--- lab-3/main.py
+++ lab-3/main.py
@@ -1,107 +1,82 @@
-class BinaryTree:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+from collections import deque
+
+with open('input.txt', "r") as file:
+    N = int(file.readline())
+    source = tuple(map(int, file.readline().split(',')))
+    destination = tuple(map(int, file.readline().split(',')))
+
+print(f"N = {N}, source = {source}, destination = {destination}")
+
+row = [2, 2, -2, -2, 1, 1, -1, -1]
+col = [-1, 1, 1, -1, 2, -2, 2, -2]
 
 
-# 3 variant
-max_counter = 0
+def is_valid(x, y, N):
+    return 0 <= x < N and 0 <= y < N
 
 
-def max_binary_tree_diameter(tree: BinaryTree) -> int:
-    if tree is None:
-        return 0
-    global max_counter
-    left_height = 0
-    right_height = 0
-    current_node = tree
-    while current_node.left is not None:
-        left_height += 1
-        current_node = current_node.left
-    current_node = tree
-    while current_node.right is not None:
-        right_height += 1
-        current_node = current_node.right
-    counter = left_height + right_height
-    if counter > max_counter:
-        max_counter = counter
-    max_binary_tree_diameter(tree.left)
-    max_binary_tree_diameter(tree.right)
-    return max_counter
+def find_min_moves(N, source, destination):
+    visited = [[False] * N for _ in range(N)]
+    queue = deque([(source[0], source[1], 0, [(source[0], source[1])])])
+    iteration = 0
+    while queue:
+        iteration += 1
 
-def max_binary_tree_diameter(tree):
-    if tree is None:
-        return 0
+        x, y, moves, path = queue.popleft()
+        if (x, y) == destination:
+            print(iteration)
+            return moves, path
 
-    max_diameter = 0
-    stack = [tree]
+        for k in range(7):
+            iteration += 1
 
-    while stack:
-        current_node = stack.pop()
-
-        left_height = 0
-        left_node = current_node
-        while left_node.left:
-            left_height += 1
-            left_node = left_node.left
-
-        right_height = 0
-        right_node = current_node
-        while right_node.right:
-            right_height += 1
-            right_node = right_node.right
-
-        max_diameter = max(max_diameter, left_height + right_height)
-
-        if current_node.left:
-            stack.append(current_node.left)
-        if current_node.right:
-            stack.append(current_node.right)
-
-    return max_diameter
+            new_x = x + row[k]
+            new_y = y + col[k]
+            if is_valid(new_x, new_y, N) and not visited[new_x][new_y]:
+                visited[new_x][new_y] = True
+                queue.append((new_x, new_y, moves + 1, path + [(new_x, new_y)]))
 
 
-def binary_tree_diameter(tree: BinaryTree, left_node: BinaryTree, right_node: BinaryTree) -> int:
-    if tree is None:
-        return 0
-
-    stack = [tree]
-
-    while stack:
-        current_node = stack.pop()
-
-        left_height = 0
-        current_left_node = current_node
-        while current_left_node.left:
-            left_height += 1
-            current_left_node = current_left_node.left
-
-        right_height = 0
-        current_right_node = current_node
-        while current_right_node.right:
-            right_height += 1
-            current_right_node = current_right_node.right
-
-        if current_right_node is right_node and current_left_node is left_node:
-            return right_height + left_height
-
-        if current_node.left:
-            stack.append(current_node.left)
-        if current_node.right:
-            stack.append(current_node.right)
+with open('output.txt', 'w') as file:
+    moves, path = find_min_moves(N, source, destination)
+    file.write(str(moves) + "\n")
+    file.write(str(path))
 
 
-root = BinaryTree(1)
-right = root.right = BinaryTree(2)
-root.left = BinaryTree(3)
-root.left.right = BinaryTree(4)
-root.left.left = BinaryTree(7)
-root.left.right.right = BinaryTree(5)
-root.left.right.right.right = BinaryTree(6)
-left = root.left.right.right.right.right = BinaryTree(12)
-root.left.left.left = BinaryTree(8)
-root.left.left.left.left = BinaryTree(9)
+def build_adjacency_list(N):
+    adjacency_list = {}
+    for x in range(N):
+        for y in range(N):
+            moves = []
+            for k in range(8):
+                new_x = x + row[k]
+                new_y = y + col[k]
+                if is_valid(new_x, new_y, N):
+                    moves.append((new_x, new_y))
+            adjacency_list[(x, y)] = moves
+    return adjacency_list
 
-print(max_binary_tree_diameter(root))
-print(binary_tree_diameter(root, left, right))
+
+def find_min_moves2(N, source, destination):
+    adjacency_list = build_adjacency_list(N)
+    print(adjacency_list)
+    visited = [[False] * N for _ in range(N)]
+    queue = deque([(source[0], source[1], 0, [(source[0], source[1])])])
+    iterations = 0
+    while queue:
+        iterations += 1
+        x, y, moves, path = queue.popleft()
+        if (x, y) == destination:
+            print(iterations)
+            return moves, path
+
+        for new_x, new_y in adjacency_list[(x, y)]:
+            if not visited[new_x][new_y]:
+                visited[new_x][new_y] = True
+                queue.append((new_x, new_y, moves + 1, path + [(new_x, new_y)]))
+
+
+with open('output2.txt', 'w') as file:
+    moves, path = find_min_moves2(N, source, destination)
+    file.write(str(moves) + "\n")
+    file.write(str(path))
