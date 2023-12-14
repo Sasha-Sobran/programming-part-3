f9b3276fdb6a4601453f8788372fff833df61121
Sasha <113774476+Sasha-Sobran@users.noreply.github.com>
2023-12-14 22:20:06 +0200

    lab 7 test

--- lab7/main.py
+++ lab7/main.py
@@ -2,4 +2,6 @@ from src.lab7 import calculate_maximum_chain, read_from_file
 
 if __name__ == "__main__":
     with open("wchain.out", "w") as file:
-        file.write(f"result: {str(calculate_maximum_chain(*read_from_file('wchain.in')))}")
+        count, words = calculate_maximum_chain(*read_from_file('wchain.in'))
+        file.write(f"result: {str(count)}\n")
+        file.write(str(words))
--- lab7/src/lab7.py
+++ lab7/src/lab7.py
@@ -8,25 +8,38 @@ def read_from_file(file) -> [int, list[str]]:
 
 def calculate_maximum_chain(words_count: int, words: list[str]):
     max_chain_lengths = {}
+    max_chain_words = {}
+    counter = 0
 
-    def max_chain_length(word):
-        if word in max_chain_lengths:
-            return max_chain_lengths[word]
-
+    for word in words:
         if len(word) == 1:
-            return 1
+            max_chain_lengths[word] = 1
+            max_chain_words[word] = [word]
 
-        max_len = 1
+    for word in words:
+        if len(word) > 1:
+            max_len = 1
+            max_chain = [word]
 
-        for i in range(len(word)):
-            temp_word = word[:i] + word[i + 1:]
-            if temp_word in words:
-                temp_len = 1 + max_chain_length(temp_word)
-                max_len = max(max_len, temp_len)
+            for i in range(len(word)):
+                counter += 1
+                temp_word = word[:i] + word[i + 1:]
 
-        max_chain_lengths[word] = max_len
-        return max_len
+                if temp_word in max_chain_lengths:
+                    temp_len = max_chain_lengths[temp_word] + 1
+                    temp_chain = max_chain_words[temp_word] + [word]
 
-    max_chain = max(max_chain_length(word) for word in words)
+                    if temp_len > max_len:
+                        max_len = temp_len
+                        max_chain = temp_chain
 
-    return max_chain
+            max_chain_lengths[word] = max_len
+            max_chain_words[word] = max_chain
+
+    max_chain_word = max(max_chain_words.values(), key=len)
+    max_chain_length = max(max_chain_lengths.values())
+    for zxc in max_chain_words:
+        if len(max_chain_words[zxc]) == 7:
+            print(max_chain_words[zxc])
+    print(counter)
+    return max_chain_length, max_chain_word
